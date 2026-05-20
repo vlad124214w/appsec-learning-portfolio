4259723920d07fe6a5046f5b0465a80fe21bc0cd
Vladislav Groshev <vlad.groshev.2015@mail.ru>
2026-05-20 23:43:51 +0400

    add real SQL injection example

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 import sqlite3
 
-def get_user(username):
-    query = "SELECT * FROM users WHERE name = '" + username + "'"
-    conn = sqlite3.connect('db.sqlite')
+def login(username, password):
+    query = f"SELECT * FROM users WHERE username = '{username}' AND password = '{password}'"
+    conn = sqlite3.connect('db.db')
     conn.execute(query)
-
-get_user("test")
